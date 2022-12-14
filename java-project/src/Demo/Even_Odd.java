3028d7f249b3f2f11d0f38127fa3180fdd72683e
Soujanya <Soujanya@sowjanya>
2022-12-14 16:42:54 +0530

    Even odd /Febbinocci

---
+++ java-project/src/Demo/Even_Odd.java
@@ -0,0 +1,31 @@
+package Demo;
+
+import java.util.Scanner;
+
+public class Even_Odd {
+
+	public static void main(String[] args) {
+		
+		
+		int n;
+		for(int i=0;i<=3;i++) {
+		System.out.println("enter the value: ");
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		
+		if(n%2==0) {
+			System.out.println("number Even");
+			
+		}else {
+			System.out.println("number Odd");
+		}
+		}
+		
+		
+		
+		
+		
+
+	}
+
+}
